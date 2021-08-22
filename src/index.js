7d4106651dc1c939c517b17611f677ca7ff86bee
smcommits <sm.anas@outlook.com>
2021-08-23 03:39:41 +0530

    Fix Conflict

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
+import dotenv from 'dotenv';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
